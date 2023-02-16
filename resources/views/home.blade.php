ba7eae947e0ef07251a3747e23b2359626691b20
giuseppemaisto <gmaisto98@gmail.com>
2023-02-16 17:07:23 +0100

    aggiunto foreach per stampa card e file footer, header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,25 @@
 </head>
 
     <body>  
-        
+        <div class="container">
+          <div class="row">
+          @foreach ($comics as $item)
+
+            <div class="col-2">
+                <div class="card">
+                
+                    <img class="card-img-top img-fluid" src="{{ print_r( $item['thumb'])}}" alt="{{ $item['series']}}">
+                    <div class="card-body">
+                        <h6>{{ $item['series']}}</h6>
+                    </div>
+                </div>
+            </div>
+               
+            @endforeach
+          
+          </div>
+
+            
+        </div>
     </body>
 </html>
